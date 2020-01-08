4d4e77f901932dd66f98ab9b7fd55968b414dc3f
Sergei Minaev <brainsburn@yandex.ru>
2020-01-09 00:47:42 +0300

    Initial

---
+++ src/bin/main.rs
@@ -0,0 +1,52 @@
+use rase::ThreadPool;
+use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::fs::File;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+	let pool = ThreadPool::new(100);
+
+	for stream in listener.incoming() {
+        //println!("stream incoming...");
+		let stream = stream.unwrap();
+        //thread::spawn(|| {
+		pool.execute(|| {
+			handle_connection(stream);
+		});
+	}
+}
+
+fn handle_connection(mut stream: TcpStream) {
+
+	let mut buffer = [0; 512];
+
+	stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+	let get = b"GET / HTTP/1.1\r\n";
+	let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+	let (status_code, filename) = if buffer.starts_with(get) {
+        ("200 OK", "goodbye.html")
+	} else if buffer.starts_with(sleep) {
+		thread::sleep(Duration::from_millis(500));
+        ("200 OK", "goodbye.html")
+	} else {
+        ("401 NOT FOUND", "goodbye.html")
+	};
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",
+                           status_code, contents.len(), contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+pub struct ThreadPool {
+	workers: Vec<Worker>,
+	sender: mpsc::SyncSender<Message>,
+}
+trait FnBox {
+	fn call_box(self: Box<Self>);
+}
+impl<F: FnOnce()> FnBox for F {
+	fn call_box(self: Box<F>) {
+		(*self)();
+	}
+}
+type Job = Box<dyn FnBox + Send + 'static>;
+
+enum Message {
+	NewJob(Job),
+	Terminate,
+}
+struct Worker {
+	id: usize,
+	thread: Option<thread::JoinHandle<()>>,
+}
+
+
+impl ThreadPool {
+	pub fn new(thread_count: usize) -> ThreadPool {
+
+        if thread_count <= 0 {
+            panic!("Number of threads should be bigger than 0.");
+        }
+
+		let (sender, receiver) = mpsc::sync_channel(1000);
+		let receiver = Arc::new(Mutex::new(receiver));
+		let mut workers = Vec::with_capacity(thread_count);
+		for id in 0..thread_count {
+			workers.push(Worker::new(id, Arc::clone(&receiver)));
+		}
+		ThreadPool {
+			workers,
+			sender,
+		}
+	}
+
+	pub fn execute<F>(&self, f: F)
+		where
+			F: FnOnce() + Send + 'static
+	{
+        //println!("execute()");
+		let job = Box::new(f);
+		//self.sender.try_send(Message::NewJob(job)).unwrap();
+		let r = self.sender.try_send(Message::NewJob(job));
+        if r.is_err() {
+            println!("R: {:?}", r);
+        }
+
+	}
+
+    pub fn kill_some_workers(count: usize) {
+        println!("Killing workers: {}", count);
+    }
+}
+
+impl Drop for ThreadPool {
+	fn drop(&mut self) {
+		println!("Sending terminate to all workers");
+		for _ in &mut self.workers {
+			self.sender.send(Message::Terminate).unwrap();
+		}
+		println!("Shutting down all workers");
+		for worker in &mut self.workers {
+			println!("Shutting down worker {}", worker.id);
+
+			if let Some(thread) = worker.thread.take() {
+				thread.join().unwrap();
+			}
+		}
+	}
+}
+
+impl Worker {
+	fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+		let thread = thread::spawn(move || {
+			loop {
+                //println!("Loop {}", id);
+				let message = receiver.lock().unwrap().recv().unwrap();
+				match message {
+					Message::NewJob(job) => {
+						//println!("Worker {} got a job; executing.", id);
+						job.call_box();
+					},
+					Message::Terminate => {
+						println!("Worker {} was told to terminate", id);
+						break;
+					},
+				}
+			}
+		});
+
+		Worker {
+			id,
+			thread: Some(thread),
+		}
+	}
+
+}

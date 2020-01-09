ca079e38dc5c8e5919b05203b8497b3f68a0768d
Sergei Minaev <brainsburn@yandex.ru>
2020-01-09 18:55:05 +0300

    Make config parser a bit less uglier
    Still don't know how to do it properly.

--- src/config_parser.rs
+++ src/config_parser.rs
@@ -5,29 +5,37 @@ use std::convert::TryFrom;
 
 pub struct Config {
     pub thread_count: usize,
+    pub test_param: usize,
 }
 
-fn get_def_config() -> Config {
-    return Config {
-        thread_count: 100,
-    };
+fn get_def_config_toml() -> toml::Value {
+    return (r#"
+        thread_count = 3
+        test_param = 5
+    "#).parse().unwrap();
+}
+
+fn get_config_param(def_config_toml: &toml::Value, user_config_toml: &toml::Value,
+                    param: &str) -> usize {
+    let toml_param = user_config_toml.get(param).unwrap_or(&def_config_toml[param]);
+    if !toml_param.is_integer() {
+        panic!("Error parsing config file: param {} should be a number.", param);
+    }
+    return usize::try_from(toml_param.as_integer().unwrap()).unwrap();
 }
 
 pub fn get_config() -> Config {
+    let mut config_params = ["thread_count", "asd"];
     let mut file = File::open("rase.toml").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let config_toml: toml::Value = contents.parse().unwrap();
-    let mut config = get_def_config();
 
-    let thread_count = config_toml.get("thread_count");
-    if thread_count.is_some() {
-        if !thread_count.unwrap().is_integer() {
-            panic!("Error parsing config file: thread_count should be a number.");
-        }
-        config.thread_count = usize::try_from(thread_count.unwrap().as_integer().unwrap()
-                                              ).unwrap();
-    }
+    let user_config_toml: toml::Value = contents.parse().unwrap();
+    let def_config_toml = get_def_config_toml();
 
+    let config = Config {
+        thread_count: get_config_param(&def_config_toml, &user_config_toml, "thread_count"),
+        test_param: get_config_param(&def_config_toml, &user_config_toml, "test_param"),
+    };
     return config;
 }

fc683e956a6ba26c9961297db8157e14747bb3de
Sergei Minaev <brainsburn@yandex.ru>
2020-01-10 17:07:45 +0300

    Try to handle some static files

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,12 @@
-use std::net::TcpListener;
+use std::thread;
+use std::fs::File;
+use std::path::{Path};
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::fs::File;
-use std::thread;
 use std::time::Duration;
+use std::net::TcpListener;
+use chunked_transfer::Encoder;
+use path_clean::{PathClean};
 use rase::ThreadPool;
 use rase::config_parser;
 
@@ -14,42 +17,105 @@ fn main() {
 	let pool = ThreadPool::new(conf.thread_count);
 
 	for stream in listener.incoming() {
-        //println!("stream incoming...");
+        let conf = conf.clone();
+
+
 		let stream = stream.unwrap();
-        //thread::spawn(|| {
-		pool.execute(|| {
-			handle_connection(stream);
+		pool.execute(move || {
+			handle_connection(stream, conf);
 		});
 	}
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, conf: config_parser::Config) {
 
 	let mut buffer = [0; 512];
-
 	stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let request_str = String::from_utf8_lossy(&buffer);
 
 	let get = b"GET / HTTP/1.1\r\n";
 	let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let s_static = String::from("GET static_url").replace("static_url",
+                                    conf.static_url.as_str());
 
-	let (status_code, filename) = if buffer.starts_with(get) {
-        ("200 OK", "goodbye.html")
-	} else if buffer.starts_with(sleep) {
-		thread::sleep(Duration::from_millis(500));
-        ("200 OK", "goodbye.html")
-	} else {
-        ("401 NOT FOUND", "goodbye.html")
-	};
+    if buffer.starts_with(s_static.as_bytes()) {
+        let fname = &get_requested_path(&request_str);
+        if !is_path_safe(fname) {
+            println!("Path is not safe: {}", fname);
+            return;
+        }
+        handle_static(stream, &fname, &conf);
+    } else {
+        let (status_code, filename) = if buffer.starts_with(get) {
+            ("200 OK", "goodbye.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_millis(500));
+            ("200 OK", "goodbye.html")
+        } else {
+            ("401 NOT FOUND", "goodbye.html")
+        };
 
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
+        let mut file = File::open(filename).unwrap();
+        let mut contents = String::new();
 
-    file.read_to_string(&mut contents).unwrap();
+        file.read_to_string(&mut contents).unwrap();
 
-    let response = format!("HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",
-                           status_code, contents.len(), contents);
+        let response = format!("HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",
+                               status_code, contents.len(), contents);
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
+
+fn handle_static(mut stream: TcpStream, fname: &str, conf: &config_parser::Config) {
+    let mut fullpath = String::from(conf.static_dir.as_str());
+    fullpath.push_str(fname);
+    let mut buf = Vec::new();
+    let mut f = match File::open(fullpath.as_str()) {
+        Ok(f) => f,
+        Err(err) => {
+            println!("Unable to open static file: {}", err);
+            return;
+        }
+    };
+    
+    f.read_to_end(&mut buf).unwrap();
+
+    let mut encoded = Vec::new();
+    {
+        let mut encoder = Encoder::with_chunks_size(&mut encoded, 1024*1024);
+        encoder.write_all(&buf).unwrap();
+    }
+    let f_len = f.metadata().unwrap().len();
+    let content_len = format!("Content-Length: {}", f_len);
+    let headers = [
+        "HTTP/1.1 200 OK",
+        "Transfer-Encoding: chunked",
+        content_len.as_str(),
+        "\r\n"
+    ];
+    let mut response = headers.join("\r\n")
+        .to_string()
+        .into_bytes();
+    response.extend(encoded);
+
+    match stream.write(&response) {
+        Ok(_) => (),
+        Err(e) => println!("Failed sending response: {}", e),
+    }
+}
+
+fn get_requested_path(request: &str) -> &str {
+    let mut path: Vec<&str> = request.lines().next().unwrap().split("GET /static/")
+        .collect();
+    path = path[1].split(" ").collect();
+    return path[0];
+}
+
+fn is_path_safe(requested_path: &str) -> bool {
+    let conf = config_parser::get_config();
+    let static_path = Path::new(&conf.static_dir);
+    let full_path = static_path.join(requested_path).clean();
+    return full_path.starts_with(static_path);
 }
--- src/config_parser.rs
+++ src/config_parser.rs
@@ -2,39 +2,77 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::convert::TryFrom;
 
-
+#[derive(Clone)]
 pub struct Config {
     pub thread_count: usize,
     pub test_param: usize,
+    pub static_dir: String,
+    pub static_url: String,
 }
 
 fn get_def_config_toml() -> toml::Value {
     return (r#"
         thread_count = 3
         test_param = 5
+        static_dir = ''
+        static_url = '/static/'
     "#).parse().unwrap();
 }
 
-fn get_config_param(def_config_toml: &toml::Value, user_config_toml: &toml::Value,
-                    param: &str) -> usize {
-    let toml_param = user_config_toml.get(param).unwrap_or(&def_config_toml[param]);
-    if !toml_param.is_integer() {
-        panic!("Error parsing config file: param {} should be a number.", param);
+fn get_config_param_num(def_config_toml: &toml::Value,
+                        user_config_toml: &toml::Value,
+                        param: &String,
+                        is_required: bool) -> usize {
+    if is_required && !user_config_toml.get(param).is_some() {
+        panic!("Error parsing config file: param {} is required.", param);
     }
-    return usize::try_from(toml_param.as_integer().unwrap()).unwrap();
+    let p = user_config_toml.get(param).unwrap_or(&def_config_toml[param]);
+    match p.as_integer() {
+        None => panic!("Error in config file: param {} should be a number.", param),
+        Some(r) => {
+            match usize::try_from(r) {
+                Err(err) => panic!(err),
+                Ok(r) => return r,
+            }
+        }
+    };
 }
 
-pub fn get_config() -> Config {
+fn get_config_param_str(def_config_toml: &toml::Value,
+                        user_config_toml: &toml::Value,
+                        param: &String,
+                        is_required: bool) -> String {
+    if is_required && !user_config_toml.get(param).is_some() {
+        panic!("Error parsing config file: param {} is required.", param);
+    }
+    let p = user_config_toml.get(param).unwrap_or(&def_config_toml[param]);
+    match p.as_str() {
+        None => panic!("Error in config file: param {} should be a string.", param),
+        Some(r) => return String::from(r),
+    };
+}
+
+pub fn get_config() ->  Config {
     let mut file = File::open("rase.toml").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
     let user_config_toml: toml::Value = contents.parse().unwrap();
     let def_config_toml = get_def_config_toml();
-
+    
     let config = Config {
-        thread_count: get_config_param(&def_config_toml, &user_config_toml, "thread_count"),
-        test_param: get_config_param(&def_config_toml, &user_config_toml, "test_param"),
+        thread_count: get_config_param_num(&def_config_toml,
+                                    &user_config_toml, 
+                                    &"thread_count".to_string(), false),
+        test_param: get_config_param_num(&def_config_toml,
+                                    &user_config_toml,
+                                    &"test_param".to_string(), false),
+        static_dir: String::from(&get_config_param_str(&def_config_toml,
+                                    &user_config_toml,
+                                    &"static_dir".to_string(), true)),
+        static_url: String::from(&get_config_param_str(&def_config_toml,
+                                    &user_config_toml,
+                                    &"static_url".to_string(), true)),
     };
     return config;
 }
